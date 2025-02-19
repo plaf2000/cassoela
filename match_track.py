512c6287ca6839833d85316170931348fd534d2e
plaf2000 <plaf2000@gmail.com>
2025-02-19 02:07:04 +0100

    Faster, better, stronger.

--- match_track.py
+++ match_track.py
@@ -1,6 +1,7 @@
 from scipy.optimize import linear_sum_assignment
 from collections import defaultdict
-from itertools import filterfalse, product
+from itertools import filterfalse, product, islice
+from matplotlib import pyplot as plt
 import re
 import pandas as pd
 import numpy as np
@@ -77,11 +78,57 @@ ixs = np.nonzero(is_weight_best)
 ixs = (np.split(ixs[1], np.unique(ixs[0], return_index=True)[1]))[1:]
 print(ixs)
 
-combs = filterfalse(lambda x: len(x) != len(set(x)), product(*ixs))
 
+def custom_prod(*iterables):
+    pools = [tuple(pool) for pool in iterables]
+
+    comb_counter = [0] * len(pools)
+    comb_counter_max = [len(pool)-1 for pool in pools]
+    pools_order = np.argsort(comb_counter_max)
+    comb_counter_max = sorted(comb_counter_max)
+
+    def inc_counter_at_pos(pos):
+        while pos >= 0:
+            if comb_counter[pos] < comb_counter_max[pos]:
+                break
+            pos -= 1
+        if pos == -1:
+            return False
+        comb_counter[pos] += 1
+        for j in range(pos+1, len(comb_counter)):
+            comb_counter[j] = 0
+        return True            
+
+    def inc_counter():
+        for i, (cc, ccm) in enumerate(zip(comb_counter[::-1], comb_counter_max[::-1])):
+            if cc < ccm:
+                return inc_counter_at_pos(len(comb_counter) - i - 1)
+
+    incremented = True
+    with open("out.txt", "w") as fp:
+        while incremented:
+            prod = [None] * len(pools)
+            for comb_pool_i, pool_i in enumerate(pools_order):
+                pool_el_i = comb_counter[comb_pool_i]
+                pool = pools[pool_i]
+                pool_el = pool[pool_el_i]
+                if pool_el in prod:
+                    incremented = inc_counter_at_pos(comb_pool_i)
+                    break
+                prod[pool_i] = pool_el
+                # print(comb_counter, [pools[i][j] for i, j in enumerate(comb_counter)], file=fp, flush=True)
+            if not None in prod:
+                incremented = inc_counter()
+                yield prod
+
+print(list(custom_prod((0,1),(0,2))))
+
+print("skibidi")
 max_combs = 10000
-for comb, comb_i in zip(combs, range(max_combs)):
-    print(comb_i, comb)
+p = islice(custom_prod(*ixs), max_combs)
+i = filterfalse(lambda x: len(x) != len(set(x)), p)
+combs = np.array(list(p))
+
         
 
 
@@ -97,7 +144,7 @@ for i_n, name in enumerate(names):
 df_combos = pd.DataFrame(combos)
 df_combos.to_csv("combos.csv")
 
-print(f"Found {comb_i+1} optimal combinations.")
+print(f"Found {len(combs)} optimal combinations.")
 
 
 
